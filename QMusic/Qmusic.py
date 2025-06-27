224441d5f36cae3311c4c3151ff57f27f4c5ba3b
IreneVillager <qingyun6@illinois.edu>
2025-06-26 22:52:59 -0500

    Update Qmusic.py

--- QMusic/Qmusic.py
+++ QMusic/Qmusic.py
@@ -5,4 +5,5 @@ st.set_page_config(
 )
 
 st.write("# Welcome to QMusic!")
-
+st.markdown("Hey there! Welcome to **QMusic!** This is an interactive simulator that maps quantum mechanical phenomena to audible sound. We use QuTip to simulate quantum spin dynamics.")
+st.markdown("**Have fun!**")
