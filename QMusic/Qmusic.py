62d039f6469fd3489c9c4eb88b764b02c2993799
IreneVillager <qingyun6@illinois.edu>
2025-08-05 09:19:01 -0500

    Delete QMusic directory

--- QMusic/Qmusic.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.set_page_config(
-    page_title="QMusic",
-)
-
-st.write("# Welcome to QMusic!")
-st.markdown("Hey there! Welcome to **QMusic!** This is an interactive simulator that maps quantum mechanical phenomena to audible sound. We use QuTip to simulate quantum spin dynamics.")
-st.markdown("**Have fun!**")
